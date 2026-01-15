77f24791282488afeb488dd14c74ca46800cb021
Abhishekh669 <nepaliabhishekh669@gmail.com>
2026-01-15 13:40:22 +0545

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	router.HEAD("/", func(c *gin.Context) {
+		c.Status(200)
+	})
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"pong": "pong"})
+	})
+
+	router.Use(cors.New(cors.Config{
+		// AllowOrigins: []string{"http://localhost:3000", "https://kitbmantra.vercel.app"},
+		// AllowAllOrigins: "",
+		AllowOrigins:     []string{"http://localhost:3000", "*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Authorization", "X-App-Token"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("❌ Server failed to start: %v", err)
+	}
+
+}
